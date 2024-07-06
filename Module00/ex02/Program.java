c714d71af5363425104003cfc04fc503b61d9187
Soufiane Elkhamlichi <05soufiane@gmail.com>
2024-07-06 17:08:08 +0100

    Module00/ex02

--- Module00/ex02/Program.java
+++ Module00/ex02/Program.java
@@ -3,39 +3,43 @@ import java.util.Scanner;
 class Program {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		int	i = 0;
+		int number = -1;
+		int i = 0;
 
-		while (true) {
-			System.out.print("-> ");
-			int	sum = 0;
-			int	number = scanner.nextInt();
-			if (number == 42)
+		while (scanner.hasNextInt() && number != 42) {
+			number = scanner.nextInt();
+			if (number == 42 || number < 2)
 				break ;
-			sum = sumOfDigits(number);
-			if (isPrime(sum))
+			if (isPrime(sumOfDigits(number)))
 				i++;
-
 		}
-		System.out.println("Count of coffee-request : " + i);
+		if (number != 42) {
+			System.out.println("illegalArgument");
+			scanner.close();
+			return ;
+		}
+		System.out.print("Count of coffee-request : ");
+		System.out.println(i);
 		scanner.close();
 	}
 
-	public static int sumOfDigits(int number)
-	{
-		if (number == 0)
-			return (0);
-		else
-			return (number % 10 + sumOfDigits(number / 10));
+	private static int sumOfDigits(int number) {
+		if (number == 0) {
+			return number;
+		}
+		return number % 10 + sumOfDigits(number / 10);
 	}
 
-	public static boolean isPrime(int number) {
-		long i = 2;
-		while (i * i <= number) {
-			if (number % i == 0) {
-				return (false);
-			}
+	private static boolean isPrime(int number) {
+		int i = 2;
+
+		if (number <= 1)
+			return false;
+		while (i * i < number) {
+			if (number % i == 0)
+				return false;
 			i++;
 		}
-		return (true);
+		return true;
 	}
 }
