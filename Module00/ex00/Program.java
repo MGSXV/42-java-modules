44b0f62032c0c6e91a9c768db49deeca8b3ea25a
Soufiane Elkhamlichi <05soufiane@gmail.com>
2024-07-06 14:36:40 +0100

    Module00/ex00

--- Module00/ex00/Program.java
+++ Module00/ex00/Program.java
@@ -6,7 +6,7 @@
 /*   By: mgs <mgs@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/16 01:34:42 by sel-kham          #+#    #+#             */
-/*   Updated: 2024/07/06 14:15:04 by mgs              ###   ########.fr       */
+/*   Updated: 2024/07/06 14:31:00 by mgs              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,21 @@ class Program
 	public static void main(String args[])
 	{
 		int		number;
+		int		res;
 
 		number = 479598;
-		System.out.println(sumOfDigits(number));
-	}
-
-	public static int sumOfDigits(int number)
-	{
-		if (number == 0)
-			return (0);
-		return (number % 10 + sumOfDigits(number / 10));
+		res = 0;
+		res += number % 10;
+		number /= 10;
+		res += number % 10;
+		number /= 10;
+		res += number % 10;
+		number /= 10;
+		res += number % 10;
+		number /= 10;
+		res += number % 10;
+		number /= 10;
+		res += number % 10;
+		System.out.println(res);
 	}
 }
