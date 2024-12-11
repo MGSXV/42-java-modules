a1166c0347056d649d7eb5ffa7e3995170c161e2
Soufiane Elkhamlichi <05soufiane@gmail.com>
2024-12-11 16:58:18 +0100

    ..

--- Module01/ex04/UserIdsGenerator.java
+++ Module01/ex04/UserIdsGenerator.java
@@ -1,70 +1,18 @@
-public class UsersArrayList implements UsersList {
-	
-	private static UsersArrayList instance;
-	private User[] users = new User[10];
+public class UserIdsGenerator {
+	static private UserIdsGenerator instance = null;
+	private long id = 0;
 
-	private UsersArrayList() {
+	private UserIdsGenerator() {
 	}
 
-	public static UsersArrayList getInstance() {
+	public static UserIdsGenerator getInstance() {
 		if (instance == null) {
-			instance = new UsersArrayList();
+			instance = new UserIdsGenerator();
 		}
 		return instance;
 	}
 
-	private void resize() {
-		User[] newUsers = new User[users.length * 2];
-		for (int i = 0; i < users.length; i++) {
-			newUsers[i] = users[i];
-		}
-		users = newUsers;
-	}
-
-	public void addUser(User user) {
-		if (user == null) {
-			return;
-		}
-		if (users[users.length - 1] != null) {
-			resize();
-		}
-		for (int i = 0; i < users.length; i++) {
-			if (users[i] == null) {
-				users[i] = user;
-				break;
-			}
-		}
-	}
-
-	public User getUserByIndex(int index) {
-		if (index < 0 || index >= users.length) {
-			throw new UserNotFoundException("User not found");
-		}
-		return users[index];
-	}
-
-	public User getUserById(long id) {
-		for (int i = 0; i < users.length; i++) {
-			if (users[i] != null && users[i].getID() == id) {
-				return users[i];
-			}
-		}
-		throw new UserNotFoundException("User not found");
-	}
-
-	public long getUsersCount() {
-		long count = 0;
-		for (int i = 0; i < users.length; i++) {
-			if (users[i] != null) {
-				count++;
-			}
-		}
-		return count;
-	}
-
-	public class UserNotFoundException extends RuntimeException {
-		public UserNotFoundException(String message) {
-			super(message);
-		}
+	public long generateId() {
+		return id++;
 	}
 }
